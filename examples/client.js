0434b85e7c86caf3dad0522c95391b1cf22282c5
Timo Derstappen <teemow@gmail.com>
2011-10-16 13:46:20 +0200

    changed paging parameter handling

--- examples/client.js
+++ examples/client.js
@@ -1,9 +1,18 @@
-var dawanda = require("dawanda").createClient("YOUR_API_KEY", "de")
+var api_key = process.env.DAWANDA_API_KEY || "YOUR_API_KEY"
+  , dawanda = require("../lib/dawanda").createClient(api_key, "de")
 dawanda.get_top_categories(function(err, result) {
   if (err) throw err
   console.log(result)
 })
 dawanda.search_users("kodanin", function(err, result) {
+  if (err) throw err
+  console.log(result)
+})
+dawanda.get_products_for_shop("2583034", function(err, result) {
+  if (err) throw err
+  console.log(result)
+})
+dawanda.get_products_for_shop("2583034", {page:2}, function(err, result) {
   if (err) throw err
   console.log(result)
 })
